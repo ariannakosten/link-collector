3c8f4643fd2cba2bdbb230ee7192deb8717d8c41
Arianna Kosten <arianna.kosten@gmail.com>
2019-08-17 07:27:15 +0000

    change welcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,30 +6,12 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "golfclubsaregreat"
-  end
-
-  get "/" do
-    erb :welcome
-  end
-
-end
-
-    
-require './config/environment'
-
-class ApplicationController < Sinatra::Base
-
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-    enable :sessions
     set :session_secret, "linksonlinksonlinks"
   end
 
 
   get '/' do 
-    erb :index
+    erb :welcome
 
   end
 
