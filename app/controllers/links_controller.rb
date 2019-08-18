63dfac7866bad2e172d8ffc733880b7274511326
Arianna Kosten <arianna.kosten@gmail.com>
2019-08-18 01:31:24 +0000

    troubleshoot errors in links/index.erb & new.erb

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,17 +1,4 @@
 class LinksController < ApplicationController
-  
-  # get "/clubs" do
-  #   redirect_if_not_logged_in 
-  #   @clubs = GolfClub.all
-  #   erb :'golf_clubs/index'
-  # end
-
-  # get "/clubs/new" do
-  #   redirect_if_not_logged_in 
-  #   @error_message = params[:error]
-  #   erb :'golf_clubs/new'
-  # end
-  
    get '/links' do
     if logged_in?
       @links = Link.all
@@ -30,46 +17,97 @@ class LinksController < ApplicationController
       redirect '/login'
     end
   end
-  
-  post '/links' do
-    if params[:link][:description] == "" || params[:link][:name] == ""
-      redirect '/links/new'
-    else
-      link = @user.links.create(params[:link])
-      link.categories << Category.find_or_create_by(title: params[:category][:title]) if !params[:category][:title].empty?
-      redirect '/links'
-    end
+
+  get "/links/:id/edit" do
+   logged_in? 
+    @error_message = params[:error]
+    @link = Link.find(params[:id])
+    erb :'links/edit'
   end
-  
-  get '/links/:id' do
-    if logged_in?
-      @link = Link.find_by_id(params[:id])
-      erb :'links/show'
-    else
-      redirect '/login'
-    end
+
+  post "/links/:id" do
+    logged_in? 
+    @link = Link.find(params[:id])
+    # unless Link.valid_params?(params)
+    #   redirect "/links/#{@link.id}/edit?error=invalid link info"
+    # end
+    @link.update(params.select{|k|k=="name" || k=="description" || k=="category"})
+    redirect "/links/#{@link.id}"
   end
-  
-  get '/links/:id/edit' do
-    if logged_in?
-      @link = Link.find_by_id(params[:id])
-      @categories = @user.categories.uniq.sort_by { |category| category.title }
-      erb :'links/edit'
-    else
-      redirect '/login'
-    end
+
+  get "/links/:id" do
+    logged_in? 
+    @link = Link.find(params[:id])
+    erb :'links/show'
   end
-  
-  patch '/links/:id' do
-    @link = Link.find_by_id(params[:id])    
-    if params[:link][:description] == "" || params[:link][:name] == ""
-       redirect "/links/#{@link.id}/edit"
-    else
-      @link.update(params[:link])
-      @link.categories << Category.find_or_create_by(title: params[:category][:title]) if !params[:category][:title].empty?
-      redirect "/links/#{@link.id}"
-    end
+
+  post "/links" do
+    logged_in? 
+    Link.create(params[:id])
+    redirect "/links"
   end
+end
+
+# ==============
+  
+  # get '/links' do
+  #   if logged_in?
+  #     @links = Link.all
+  #     erb :'links/index'
+  #   else
+  #     redirect '/login'
+  #   end
+  # end
+  
+  # get '/links/new' do
+  #   if logged_in?
+  #     @categories = Category.all
+  #     #@categories = @user.categories.uniq.sort_by { |category| category.title }
+  #     erb :'links/new'
+  #   else 
+  #     redirect '/login'
+  #   end
+  # end
+  
+  # # post '/links' do
+  #   if params[:link][:description] == "" || params[:link][:name] == ""
+  #     redirect '/links/new'
+  #   else
+  #     link = @user.links.create(params[:link])
+  #     link.categories << Category.find_or_create_by(title: params[:category][:title]) if !params[:category][:title].empty?
+  #     redirect '/links'
+  #   end
+  # end
+  
+  # get '/links/:id' do
+  #   if logged_in?
+  #     @link = Link.find_by_id(params[:id])
+  #     erb :'links/show'
+  #   else
+  #     redirect '/login'
+  #   end
+  # end
+  
+  # get '/links/:id/edit' do
+  #   if logged_in?
+  #     @link = Link.find_by_id(params[:id])
+  #     @categories = @user.categories.uniq.sort_by { |category| category.title }
+  #     erb :'links/edit'
+  #   else
+  #     redirect '/login'
+  #   end
+  # end
+  
+  # patch '/links/:id' do
+  #   @link = Link.find_by_id(params[:id])    
+  #   if params[:link][:description] == "" || params[:link][:name] == ""
+  #     redirect "/links/#{@link.id}/edit"
+  #   else
+  #     @link.update(params[:link])
+  #     @link.categories << Category.find_or_create_by(title: params[:category][:title]) if !params[:category][:title].empty?
+  #     redirect "/links/#{@link.id}"
+  #   end
+  # end
   
   delete '/links/:id' do
     @link = Link.find_by_id(params[:id])
@@ -77,4 +115,4 @@ class LinksController < ApplicationController
     redirect '/links'
   end
   
-end
+# end
